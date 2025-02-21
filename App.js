43d8a5b8c5572dfa5791f621d2807ab90ec26efc
StevenBalagueraP <161085601+StevenBalagueraP@users.noreply.github.com>
2025-02-20 19:24:50 -0500

    Revert "Merge pull request #8 from chrisfalla/LeymarUrbano"
    This reverts commit d48a193692f57871eaf4524697d7d13823a6c21e, reversing
    changes made to bc1f30af145e26afdff89f96ed55f7a793401ade.

--- App.js
+++ App.js
@@ -1,6 +1,28 @@
-import React from 'react';
-import LoginScreen from './src/screens/LoginScreen';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
+import icon from './assets/icon.png';
 export default function App() {
-  return <LoginScreen />;
+  return (
+    <View style={styles.container}>
+      <Text style={{ width: 200 }}>bienvenidos a la app de turismo de yopal</Text>
+      <StatusBar style="auto" />
+      <Text>hola mundo</Text>
+      {/* <Image blurRadius={1} source={icon} style={{
+        width: 120,
+        height: 300,
+        resizeMode: 'contain'
+      }} /> */}
+    </View>
+  );
 }
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#ffe',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
