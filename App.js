35a6f59abc1b125da19aabe3a37e189e1b3c0227
StevenBalagueraP <161085601+StevenBalagueraP@users.noreply.github.com>
2025-02-21 21:14:35 -0500

    CategoriaLugares/BotonesCategorias solucionando conflictos

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import LoginScreen from './src/screens/LoginScreen';
+import { SafeAreaView, StyleSheet } from 'react-native';
 import Category from './src/components/Category';
 
 export default function App() {
